2d46aead7ac3014f387bd16e0b19abf239939527
Bruce Li <pengyuli@chicagobooth.edu>
2019-04-07 02:08:26 +0000

    start at 4.2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+    
+    
+    #returns full page title on a per-page basis
+    #page_title default value is '', aka if full_title is not given an argument
+    #the argument will be ''
+    def full_title(page_title = '')
+        base_title = "Ruby on Rails Tutorial Sample App"
+        if page_title.empty?
+            # if there is no page title, then we just return the base title
+            return base_title
+        else
+            #string addition
+            return page_title + " | " + base_title
+        end
+    end
+    
 end
