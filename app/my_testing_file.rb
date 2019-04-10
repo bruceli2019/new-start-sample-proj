e885ac2f0b61526f16a624870e5861731b5ddde4
Bruce Li <pengyuli@chicagobooth.edu>
2019-04-10 05:17:42 +0000

    start at 4.3.2

--- app/my_testing_file.rb
+++ app/my_testing_file.rb
@@ -14,3 +14,19 @@ puts palindrome_test("")
 
 #note that palindrome_test returns a value instead of puts it, so .nil? returns false
 puts palindrome_test("bruceli").nil?
+
+#exercise 4.3.1
+a = "A man, a plan, a canal, Panama".split(', ')
+puts a
+
+s = a.join
+puts s
+
+k = s.split
+l = k.join
+puts palindrome_test(l)
+puts palindrome_test(l.downcase)
+
+puts ('a'..'z').to_a[6]
+puts ('a'..'z').to_a.reverse[6]
+puts ('a'..'z').to_a[-7]
