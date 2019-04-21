f2110bb78928a90993853e902fe1334745ce0587
Bruce Li <pengyuli@chicagobooth.edu>
2019-04-21 03:51:07 +0000

    start at 6.2.5

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -33,6 +33,24 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
     
+  test "email validation should accept valid addresses" do
+    #list some valid addresses to make sure the validation isn't screwing up
+    valid_addresses = %w[user@example.com USER@foo.com A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
   
+    valid_addresses.each do |address|
+      @user.email = address
+      assert @user.valid?, "#{address.inspect} should be valid" #.inspect returns a literal representation of the object it's called on
+    end
+  end
+  
+  test "email validation should not accept invalid addresses" do
+    #list some invalid addresses
+    invalid_addresses = %w[user@example,com user_at_foo.org user@example..com user.name@example. foo@bar_baz.com foo@bar+baz.com]
+  
+    invalid_addresses.each do |address|
+      @user.email = address
+      assert_not @user.valid?, "#{address.inspect} should be invalid" #assert_not returns true when @user.valid? returns false (i.e. user is invalid)
+    end
+  end
   
 end
