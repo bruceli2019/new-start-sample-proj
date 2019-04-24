6aeb9d4960b1609dae1c5821ebc63f5122543cdf
Bruce Li <pengyuli@chicagobooth.edu>
2019-04-24 14:05:50 +0000

    start 7.2.2

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
 
   get '/signup', to: 'users#new'
+  
+  resources :users #introduces full REST architecture (create, edit, update, destroy, etc.)
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
